3eb84aff506877e84e2bbc1294cb0f3744b4541a
Joel McCoy <joelmccoy+github@gmail.com>
2018-05-13 22:56:36 -0400

    Move some Wordnik/Twitter behavior to modules, favor async...await over Promise chains where sensible

--- index.js
+++ index.js
@@ -1,89 +1,68 @@
 #!/usr/bin/env node
-
 'use strict';
 
+// Replace Math.random() with MT-based substitute:
+Math.random = require('./lib/mt-rng');
+// General requirements
+const _ = require('lodash');
+const P = require('bluebird');
+const exec = require('child-process-promise').exec;
+const os = require('os');
+const path = require('path');
+const fs = P.promisifyAll(require('fs'));
+// Read API keys etc
+const creds = require('./credentials');
+// Twitter interface
+const Twit = require('./lib/twithelper');
+const T = new Twit(creds.live);
+// For working with our words
+const WordHelper = require('./lib/wordhelper');
+const W = new WordHelper(creds);
+
+// Settings/configuration
 const FONT_TO_USE = "Ubuntu-Condensed";
 const FONT_COLOR = "white";
 const FONT_SIZE = 27;
 const IMAGEMAGICK_BINARY = (process.env.IMAGEMAGICK_BINARY || "convert").replace(/\\{2,}/g, "/");
-
-// Replace Math.random() with MT-based substitute:
-Math.random = require('./mt-rng');
-
-const _ = require('lodash');
-const os = require('os');
-const path = require('path');
-const exec = require('child-process-promise').exec;
-const P = require('bluebird');
-const fs = P.promisifyAll(require('fs'));
-const request = P.promisifyAll(require('request'));
-const wordfilter = require('wordfilter')
-// Twitter interface
-const creds = require('./credentials');
-const Twit = require('twit');
-Twit.prototype.postMediaChunkedAsync = P.promisify(Twit.prototype.postMediaChunked);
-const REST = new Twit(creds.live);
-// RiTaJS
-const lex = new require('rita').RiLexicon();
-const rw = lex.randomWord.bind(lex); // useful if you want to skip Wordnik
-const isAdverb = lex.isAdverb.bind(lex);
-
 const TMP_FILE = path.join(os.tmpdir(), 'this.png');
 
-function isntAdverb(w) {
-  if (_.words(w).length > 1) {
-    return true;
-  } else if (isAdverb(w)) {
-    return false;
-  } else {
-    return true;
-  }
+function areRunningWindows() {
+  return ~os.platform().indexOf('win')
 }
 
-function getRandomWord() {
-  let target = `http://api.wordnik.com/v4/words.json/randomWord` + 
-    `?includePartOfSpeech=noun&excludePartOfSpeech=adverb&minCorpusCount=3500&maxLength=12` +
-    `&api_key=${creds.wordnik.api_key}`
-  return request.getAsync({url: target, json: true}).
-  then(res=>res.body.word);
-}
-
-function getRelatedWord(srcWord) {
-  let target = `http://api.wordnik.com/v4/word.json/${srcWord}/relatedWords` +
-    `?useCanonical=true&relationshipTypes=same-context&limitPerRelationshipType=40` +
-    `&api_key=${creds.wordnik.api_key}`
-  return request.getAsync({url: target, json: true}).
-  then(res => _(res.body).flatMap("words").filter(isntAdverb).value() ).
-  then(related => related.length < 1 ? getRandomWord() : _.sample(related)).
-  then(related => {return {srcWord: srcWord, relWord: related}} );
-}
-
-function getWordPair() {
-  return getRandomWord().
-  then(srcWord => getRelatedWord(srcWord))
-}
-
-getWordPair().
-  then(words => {
-    if (_.isUndefined(words.relWord) || _.isUndefined(words.srcWord) || wordfilter.blacklisted(words.srcWord) || wordfilter.blacklisted(words.relWord) ) {
-      return getWordPair()
-    } else {
-      return words
-    }
-  }).
-  tap(console.log).
-  then(words => {
-    if (~os.platform().indexOf('win') ) {
+function makeConvertCall(words) {
+    if ( areRunningWindows() ) {
       return `"${IMAGEMAGICK_BINARY}" ./isthis.png -font "${FONT_TO_USE}" -fill ${FONT_COLOR} -background none -pointsize ${FONT_SIZE} ( label:"${words.srcWord}" -rotate -8 -geometry +430+150 ) -composite ( label:"${words.relWord}" -geometry +310+430 ) -composite "${TMP_FILE}"`
     } else {
       return `${IMAGEMAGICK_BINARY} ./isthis.png -font "${FONT_TO_USE}" -fill ${FONT_COLOR} -background none -pointsize ${FONT_SIZE} \\( label:"${words.srcWord}" -rotate -8 -geometry +430+150 \\) -composite \\( label:"${words.relWord}" -geometry +310+430 \\) -composite "${TMP_FILE}"`
     }
-  }).
-  then(imCall => exec(imCall)).
-  then(ret=>REST.postMediaChunkedAsync({file_path: TMP_FILE})).
-  then(r=>REST.post('statuses/update', {
-          status: '',
-          media_ids: [r.media_id_string]
-        })).
-  then(res=>console.log(`ISTHIS twote:${res.data.id_str}`)).
-  catch(console.error);
+}
+
+// srcOpts and relOpts are either objects with Wordnik params, or strings of each word to use
+async function tweetMeme(srcOpts, relOpts, force) {
+  let wordPair, convertCmd, convertRet, status, altText;
+
+  wordPair = await W.getUsableWordPair(srcOpts, relOpts, force);
+
+  // Fill in our tweet's text fields:
+  status = ``;
+  altText = `Cartoon man in glasses holding book looking at a butterfly labeled '${wordPair.srcWord}', asking "Is this ${wordPair.relWord}"`;
+
+  // Construct the ImageMagick convert call, then run it to generate PNg at TMP_FILE
+  convertCmd = makeConvertCall(wordPair);
+  convertRet = await exec(convertCmd);
+
+  console.log(`tweeting using ${JSON.stringify(wordPair)}`);
+  // Now upload the image, assign its alt_text metadata then send the tweet, returning a Promise:
+  return T.makeTweet(status, TMP_FILE, altText);
+}
+
+// Do it, supporting command line overrides:
+let srcWord = process.argv[2];
+let relWord = process.argv[3];
+let force = !!process.argv[4];
+
+console.log(force);
+tweetMeme(srcWord, relWord, force).
+then(res=>console.log(`ISTHIS twote: ${res.data.id_str}`)).
+catch(console.error);
